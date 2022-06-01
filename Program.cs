d1f66574ffd2b29dbfceff03af343d452063e025
Darko Blazhevski <darkoblazhevski@outlook.com>
2022-06-01 12:30:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("abu graib");
+
+            //add
+            int[,] matrica = new int[8, 8];
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    matrica[i, j] = 1;
+                }
+            }
+            //priunt
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(matrica[i, j]);
+                    Console.Write("   ");
+                }
+            }
+        }
+    }
+}
