271e03cce3b751f3ede45e035c9d56fe46165eef
Darko Blazhevski <darkoblazhevski@outlook.com>
2022-06-01 14:43:56 +0200

    row&column logic

--- Program.cs
+++ Program.cs
@@ -15,10 +15,48 @@ namespace github
             {
                 for (int j = 0; j < 8; j++)
                 {
-                    matrica[i, j] = 1;
+                    matrica[i, j] = 0;
                 }
             }
-            //priunt
+            matrica[0, 0] = 9;
+            //pecatenje(matrica);
+            for (int i = 0; i < 8; i++)
+            {
+
+                for (int j = 0; j < 8; j++)
+                {
+                    if (matrica[i,j]==9)
+                    {
+                        matrica[i, 0]++;
+                        matrica[i, 1]++;
+                        matrica[i, 2]++;
+                        matrica[i, 3]++;
+                        matrica[i, 4]++;
+                        matrica[i, 5]++;
+                        matrica[i, 6]++;
+                        matrica[i, 7]++;
+
+                        matrica[0, j]++;
+                        matrica[1, j]++;
+                        matrica[2, j]++;
+                        matrica[3, j]++;
+                        matrica[4, j]++;
+                        matrica[5, j]++;
+                        matrica[6, j]++;
+                        matrica[7, j]++;
+                        matrica[i, j]--;
+                        matrica[i, j]--;
+
+
+
+                    }
+                }
+            }
+            pecatenje(matrica);
+        }
+        
+        static void pecatenje(int[,] matrica)
+        {
             for (int i = 0; i < 8; i++)
             {
                 Console.WriteLine("\n");
